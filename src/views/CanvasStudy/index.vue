eebc02a36190f348ec4ea9db455e3250b96e1d5c
Sneanken <924393527@qq.com>
2020-08-07 14:30:22 +0800

    style：add normalize.css

---
+++ src/views/CanvasStudy/index.vue
@@ -0,0 +1,14 @@
+<template>
+  <canvas></canvas>
+</template>
+
+<script>
+export default {
+  name: "CanvasStudy",
+  data() {
+    return {};
+  }
+};
+</script>
+
+<style lang="less" scoped></style>
